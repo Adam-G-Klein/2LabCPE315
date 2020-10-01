f37c7669334f43182e2750aba2c23ecc7caa2d4a
Adam Klein <agklein@gmail.com>
2020-10-01 16:13:08 -0700

    Init commit, Prints out test file and has LookupTable

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BinaryConvertRunner bn = new BinaryConvertRunner(args);
+        bn.run();
+
+    }
+}
