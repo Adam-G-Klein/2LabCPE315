c590165941d7700511a7b01a8df3a0fa5f1e1e91
slaterdev1 <slaterdev1@gmail.com>
2020-10-03 13:59:53 -0700

    mem instructions

---
+++ src/com/binaryconvert/instructions/MemTypeInstruction.java
@@ -0,0 +1,48 @@
+package com.binaryconvert.instructions;
+
+import com.binaryconvert.InstructionLookup;
+
+public class MemTypeInstruction implements Instruction {
+    /***
+     * Data class for the Mem type instructions:
+     *  and, or, add, sub
+     *
+     */
+
+    private String ins;
+    private String rs;
+    private String rt;
+    private String offset;
+
+    public ShiftTypeInstruction(String insStr){
+        String[] tokens = insStr.split(",");
+        String[] insTokens = tokens[0].split("\\$");
+        ins = insTokens[0];
+        rt = tokens[0].substring(ins.length());
+        String[] subTokens = tokens[1].split("(");
+        offset = subTokens[0];
+        rs = subTokens[1].substring(subTokens[1].length() - 1);
+
+    }
+
+    public String toBinary(){
+        StringBuilder res = new StringBuilder();
+        res.append(InstructionLookup.getOpCode(ins) + " ");
+        res.append(rs + " ");
+        res.append(InstructionLookup.getReg(rt) + " ");
+        res.append(String.format("%16s",
+                Integer.toBinaryString(Integer.parseInt(offset))
+                        .replace(' ', '0')) + " ");
+        return res.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "ShiftTypeInstruction{" +
+                "ins='" + ins + '\'' +
+                ", rs='" + rs + '\'' +
+                ", rt='" + rt + '\'' +
+                ", offset='" + offset + '\'' +
+                '}';
+    }
+}
