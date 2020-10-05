1117e22969dc6ed3d091825c8d2779cc87b8ad57
Adam Klein <agklein@gmail.com>
2020-10-05 11:10:19 -0700

    Fixed java 11 build  issue

--- src/com/binaryconvert/Main.java
+++ src/com/binaryconvert/Main.java
@@ -5,6 +5,5 @@ public class Main {
     public static void main(String[] args) {
         BinaryConvertRunner bn = new BinaryConvertRunner(args);
         bn.run();
-
     }
 }
--- src/com/binaryconvert/instructions/InstructionFactory.java
+++ src/com/binaryconvert/instructions/InstructionFactory.java
@@ -9,30 +9,27 @@ public class InstructionFactory {
 
     private static final List<String> rTypes = Arrays.asList("and", "or", "add", "sub", "slt");
     private static final List<String> iTypes = Arrays.asList("addi");
-    private static final List<String> jumpTypes = Arrays.asList("j", "jr", "jal");
+    private static final List<String> jumpTypes = Arrays.asList("j", "jal");
+    private static final List<String> jrType = Arrays.asList("jr");
     private static final List<String> branchTypes = Arrays.asList("beq", "bne");
     private static final List<String> shiftTypes = Arrays.asList("sll");
     private static final List<String> memTypes = Arrays.asList("sw", "lw");
 
     public static Instruction createInstruction(Integer pcCount, String instructionLine){
         String ins = getInsFromLine(instructionLine);
-        switch(ins){
-            case "and", "or", "add", "sub", "slt" :
-                return new RTypeInstruction(ins, instructionLine);
-            case "addi":
-                return new ITypeInstruction(ins, instructionLine);
-            case "j", "jal":
-                return new JTypeInstruction(ins, instructionLine);
-            case "jr":
-                return new JrTypeInstruction(ins, instructionLine);
-            case "beq", "bne":
-                return new BranchTypeInstruction(ins, instructionLine, pcCount);
-            case "sll":
-                return new ShiftTypeInstruction(ins, instructionLine);
-            default:
-                return new InvalidInstruction(ins);
-        }
-
+        if(rTypes.contains(ins))
+            return new RTypeInstruction(ins, instructionLine);
+        if(iTypes.contains(ins))
+            return new ITypeInstruction(ins, instructionLine);
+        if(jumpTypes.contains(ins))
+            return new JTypeInstruction(ins, instructionLine);
+        if(jrType.contains(ins))
+            return new JrTypeInstruction(ins, instructionLine);
+        if(branchTypes.contains(ins))
+            return new BranchTypeInstruction(ins, instructionLine, pcCount);
+        if(shiftTypes.contains(ins))
+            return new ShiftTypeInstruction(ins, instructionLine);
+        return new InvalidInstruction(ins);
     }
 
     public static String getInsFromLine(String instructionLine){
