abd574738098087a78fd0b651e9e7dae3167a5dd
Adam Klein <agklein@gmail.com>
2020-10-05 11:19:09 -0700

    rename main

--- src/com/binaryconvert/lab2.java
+++ src/com/binaryconvert/lab2.java
@@ -1,6 +1,6 @@
 package com.binaryconvert;
 
-public class Main {
+public class lab2 {
 
     public static void main(String[] args) {
         BinaryConvertRunner bn = new BinaryConvertRunner(args);
